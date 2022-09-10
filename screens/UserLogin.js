f4f8c25cb7319dfb329c93cfb8acb1f723be7416
mücahit kaya <mucahit.kayaeee@hotmail.com>
2022-09-10 16:55:00 +0300

    new screen added

---
+++ screens/UserLogin.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+function UserLogin(params) {
+  return <View></View>;
+}
+
+export default UserLogin;
+
+const styles = StyleSheet.create({
+  container: {},
+});
